2bb43c764480827b7773d8270ef4f803ad2836e3
Kristin Barr <kristinbarr@gmail.com>
2019-06-22 00:45:36 -0400

    carousel - group optimizing + explanation comments

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,40 +1,37 @@
 class Carousel {
   constructor (carousel) {
+    // creating a copy of the carousel div to recieve cool extra functionality
     this.carousel = carousel
-    this.leftButton = this.carousel.querySelector('.carousel .left-button')
-    this.rightButton = this.carousel.querySelector('.carousel .right-button')
-    this.images = this.carousel.querySelectorAll('.carousel img')
+    // create selectors for left and right buttons (from carousel element)
+    this.leftButton = this.carousel.querySelector('.left-button')
+    this.rightButton = this.carousel.querySelector('.right-button')
+    // select a Nodelist of the carousel images (from carousel element)
+    this.images = this.carousel.querySelectorAll('img')
+    // we're starting the index at 0. it will increment/decr when pic changes
     this.currentIndex = 0
-
+    // default image set to 0th index in nodelist by making it visible
     this.images[this.currentIndex].style.display = 'inline-block'
-
-    // if (this.currentIndex > this.images.length-1) {
-    //   this.currentIndex = 0
-    // }
-
+    // event listeners set to activate methods in this class
     this.leftButton.addEventListener('click', () => this.previousSlide())
     this.rightButton.addEventListener('click', () => this.nextSlide())
+    // example of explicit binding
+    // this.leftButton.addEventListener('click', this.previousSlide.bind(this))
   }
 
   previousSlide() {
-    // MAKE NEW IMAGE VISIBLE
-    // subtract 1 from index
-    // console.log(this.currentIndex, this.images.length)
+    // use current index to change current image styling to be hidden
+    this.images[this.currentIndex].style.display = 'none'
+    // decrement current index by 1 because we're going to the previous slide
     this.currentIndex -= 1
+    // conditional if we are at the end of the list, start over
     if (this.currentIndex < 0) {
+      // this.images.length-1 is more dynamic than saying '4'
       this.currentIndex = this.images.length-1
     }
-    // console.log('here', this.currentIndex, this.images.length)
-    // change style to visible
+    // change of image we want to show style to visible
     this.images[this.currentIndex].style.display = 'inline'
 
-    // MAKE OLD IMAGE DISPLAY NONE
-    let oldIdx = this.currentIndex-1
-    if (this.currentIndex >= this.images.length) {
-      oldIdx = 0
-    }
-    this.images[oldIdx].style.display = 'none'
-
+    // future animaion to be configured
     // TweenLite.to(curImg, 1.5, {
     //   ease: SlowMo.ease.config(0.5, 0.4, false),
     //   y: -500
@@ -42,8 +39,8 @@ class Carousel {
   }
 
   nextSlide() {
-    // MAKE NEW IMAGE VISIBLE
-    // add 1 to index
+    this.images[this.currentIndex].style.display = 'none'
+    // for this method, increment the current index to go to next node/image
     this.currentIndex += 1
     if (this.currentIndex >= this.images.length) {
       this.currentIndex = 0
@@ -55,24 +52,21 @@ class Carousel {
     //   ease: SlowMo.ease.config(0.5, 0.4, false),
     //   y: -500
     // })
-
-    // MAKE OLD IMAGE DISPLAY NONE
-    let oldIdx = this.currentIndex-1
-    if (this.currentIndex === 0) {
-      oldIdx = this.images.length-1
-    }
-    this.images[oldIdx].style.display = 'none'
   }
 
   autoNext() {
-    // increment index by 1 every 3 sec
-    window.setTimeout(this.nextSlide(), 3000)
+    // future method to be configured to call nextSlide() every 3 sec
+    // return window.setTimeout(this.nextSlide(), 3000)
   }
 
 }
 
+// select the whole carousel element so we can add functionality
 let carousel = document.querySelector('.carousel');
+// instantiate the class constructor function and give our 'dumb' component
 new Carousel(carousel)
+
+
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
   1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
   2. You will need to grab a reference to all of the images
@@ -81,6 +75,3 @@ new Carousel(carousel)
   5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
   6. Have fun!
 */
-
-// let myCarousel = document.querySelector('.carousel')
-// let carousel = new Carousel(carousel)
