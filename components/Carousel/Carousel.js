efc08c3db494367f92b866eeade092a5ee15e308
Kristin Barr <kristinbarr@gmail.com>
2019-06-23 11:31:08 -0400

    added working auto timer

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -10,12 +10,16 @@ class Carousel {
     // we're starting the index at 0. it will increment/decr when pic changes
     this.currentIndex = 0
     // default image set to 0th index in nodelist by making it visible
-    this.images[this.currentIndex].style.display = 'inline-block'
+    this.images[this.currentIndex].style.display = 'inline'
     // event listeners set to activate methods in this class
     this.leftButton.addEventListener('click', () => this.previousSlide())
     this.rightButton.addEventListener('click', () => this.nextSlide())
     // example of explicit binding
     // this.leftButton.addEventListener('click', this.previousSlide.bind(this))
+
+    window.setInterval(()=> {
+      return this.nextSlide()
+    }, 5000)
   }
 
   previousSlide() {
@@ -30,35 +34,34 @@ class Carousel {
     }
     // change of image we want to show style to visible
     this.images[this.currentIndex].style.display = 'inline'
+    // TweenLite.from(this.images[this.currentIndex], 1.5, {
+    //   ease: Power4.easeOut, x: -500 })
 
     // future animaion to be configured
-    // TweenLite.to(curImg, 1.5, {
-    //   ease: SlowMo.ease.config(0.5, 0.4, false),
-    //   y: -500
-    // })
   }
 
   nextSlide() {
-    this.images[this.currentIndex].style.display = 'none'
+    const beforeImg = this.images[this.currentIndex]
+    // console.log('before', beforeImg)
+
     // for this method, increment the current index to go to next node/image
     this.currentIndex += 1
     if (this.currentIndex >= this.images.length) {
       this.currentIndex = 0
     }
-    const curImg = this.images[this.currentIndex]
-    curImg.style.display = 'inline'
 
-    // TweenLite.to(curImg, 1.5, {
-    //   ease: SlowMo.ease.config(0.5, 0.4, false),
-    //   y: -500
-    // })
+    this.images[this.currentIndex].style.display = 'inline'
+
+    // TweenLite.from(this.images[this.currentIndex], 1, {
+    //   ease: Power4.easeOut, opacity: 0, x: 500 })
+
+      // console.log('after', beforeImg, this.images[this.currentIndex])
+
+      // TweenLite.to(beforeImg, 1, {
+      //   ease: Power4.easeOut, opacity: 0, x: -100 })
+
+    beforeImg.style.display = 'none'
   }
-
-  autoNext() {
-    // future method to be configured to call nextSlide() every 3 sec
-    // return window.setTimeout(this.nextSlide(), 3000)
-  }
-
 }
 
 // select the whole carousel element so we can add functionality
